13930e811532b647daa14f6b4bc7be254b63eeae
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-07-17 20:03:21 -0300

    trabalhando com listas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { db } from "./firebaseConnection";
-import { doc, setDoc, collection, addDoc, getDoc } from "firebase/firestore";
+import { doc, setDoc, collection, addDoc, getDoc, getDocs } from "firebase/firestore";
 import "./app.css";
 import { useState } from "react";
 
@@ -7,7 +7,10 @@ function App() {
   const [titulo, setTitulo] = useState('');
   const [autor, setAutor] = useState('');
 
+  const [posts, setPosts] = useState([]);
+
   async function handleAdd() {
+
     // await setDoc(doc(db, "posts", "12345"), {
     //   titulo: titulo,
     //   autor: autor,
@@ -34,21 +37,42 @@ function App() {
   }
 
   async function buscarPost() {
-    const postRef = doc(db, "posts", "12345")
 
-    await getDoc(postRef)
+    // const postRef = doc(db, "posts", "12345")
+
+    // await getDoc(postRef)
+    //   .then((snapshot) => {
+    //     setAutor(snapshot.data().autor)
+    //     setTitulo(snapshot.data().titulo)
+    //   })
+    //   .catch(() => {
+    //     console.log("ERRO AO BUSCAR!");
+    //   })
+
+    const postsRef = collection(db, "posts")
+    await getDocs(postsRef)
       .then((snapshot) => {
-        setAutor(snapshot.data().autor)
-        setTitulo(snapshot.data().titulo)
+        let lista = [];
+
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            titulo: doc.data().titulo,
+            autor: doc.data().autor,
+          })
+        })
+
+        setPosts(lista);
+
       })
-      .catch(() => {
-        console.log("ERRO AO BUSCAR!");
+      .catch((error) => {
+        console.log('DEU ALGUM ERRO AO BUSCAR!')
       })
   }
 
   return (
     <div>
-      <h1>HELLO WORLD! =)</h1>
+      <h1>ReactJS + Firebase</h1>
 
       <div className="container">
         <label>
@@ -59,10 +83,22 @@ function App() {
         <label>
           Autor:
         </label>
-        <input type="text" placeholder="Autor do post/" value={autor} onChange={(e) => setAutor(e.target.value)} /> <br />
+        <input type="text" placeholder="Autor do post" value={autor} onChange={(e) => setAutor(e.target.value)} /> <br />
 
         <button onClick={handleAdd}>Cadastrar</button> <br />
         <button onClick={buscarPost}>Buscas posts</button>
+
+        <ul>
+          {posts.map((post) => {
+            return (
+              <li key={post.id}>
+                <span>Titulo: {post.titulo}</span> <br />
+                <span>Autor: {post.autor}</span> <br /> <br />
+              </li>
+            )
+          })}
+        </ul>
+
       </div>
     </div>
 
